3439bab3f2d5aa7cb652bfc13ff7c3f508ddd3ce
Viorel Stirbu <viorels@gmail.com>
2012-12-09 14:05:50 +0200

    add test for universe with 4 cells in block shape

--- tests.py
+++ tests.py
@@ -3,15 +3,10 @@ import unittest
 from life import evolve_universe, cell_transition
 
 
-class LifeTest(unittest.TestCase):
+class GameRulesTest(unittest.TestCase):
     def setUp(self):
-        self.empty_universe = set()
         self.live_cell_state = True
         self.dead_cell_state = False
-
-    def test_empty_universe_evolves_into_empty_universe(self):
-        new_universe = evolve_universe(self.empty_universe)
-        self.assertEqual(new_universe, self.empty_universe)
 
     def test_rule_1_underpopulation(self):
         for neighbours in (0, 1):
@@ -41,6 +36,23 @@ class LifeTest(unittest.TestCase):
             new_state = cell_transition(state=self.dead_cell_state,
                                         neighbours=neighbours)
             self.assertEqual(new_state, self.dead_cell_state)
+
+
+class UniverseTest(unittest.TestCase):
+    def setUp(self):
+        self.empty_universe = set()
+        self.block_universe = set([(0, 0), (0, 1), (1, 0), (1, 1)])
+        self.live_cell_state = True
+        self.dead_cell_state = False
+
+    def test_empty_universe_evolves_into_empty_universe(self):
+        new_universe = evolve_universe(self.empty_universe)
+        self.assertEqual(new_universe, self.empty_universe)
+
+    def test_universe_with_block_evolves_into_block(self):
+        new_universe = evolve_universe(self.block_universe)
+        self.assertEqual(new_universe, self. block_universe)
+
 
 if __name__ == '__main__':
     unittest.main()
