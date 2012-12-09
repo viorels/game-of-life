c4f1a53fc6cd6e0ecb0f169772819a827054ce81
Viorel Stirbu <viorels@gmail.com>
2012-12-09 15:01:05 +0200

    implement live neighbours count

--- life.py
+++ life.py
@@ -5,6 +5,9 @@ def evolve_universe(universe):
 
 
 def cell_transition(state, neighbours):
+    """Implements game rules and returns the new state of the cell based on
+    previous conditons passed as arguments"""
+
     if state is True:               # previous state is live
         if neighbours < 2:          # underpopulation
             return False
@@ -18,3 +21,18 @@ def cell_transition(state, neighbours):
         else:                       # no reproduction
             return False
 
+
+def all_cell_neighbours(cell):
+    """Return coordinates for all neighbours of the given cell, dead or alive"""
+
+    x, y = cell
+    offsets = ((-1,  1), (0,  1), (1,  1),
+               (-1,  0),          (1,  0),
+               (-1, -1), (0, -1), (1, -1))
+    return [(x + ox, y + oy) for ox, oy in offsets]
+
+
+def cell_neighbours_count(cell, universe):
+    """Count the live neighbour cells of the given cell in the given universe"""
+
+    return len(set(all_cell_neighbours(cell)) & universe)
